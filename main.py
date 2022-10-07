ed671c24535293acedbe7c32b9de68fda3c92154
Shiikama <dodon.vlad@mail.ru>
2022-10-07 10:55:20 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 print('Welcome, colleague!')
-list_1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-c = 0  # permutation counter
+file = open("numbers.data")
+file_with_lines = file.readlines()
+file_with_lines = (file_with_lines[0].split())
+list_1 = []
+for line in file_with_lines:
+    e = int(line)
+    list_1.append(e)
+    c = 0  # permutation counter
 for b in range(1, len(list_1)):
     for i in range(len(list_1) - 1, 1, -1):
         for x in range(len(list_1) - 1, 1, -1):
@@ -11,5 +17,5 @@ for b in range(1, len(list_1)):
                     list_1[i - x] = a
                     c += 1
                 break
-print(c)
+                print(c)
 print(f'List {list_1}')
